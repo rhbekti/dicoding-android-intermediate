01407207cce899a7b0f88f45db55e356416ec142
Rahman Pambekti <67356011+rhbekti@users.noreply.github.com>
2023-10-08 16:57:05 +0700

    Add: MyWidgets (#10)

---
+++ advanced-ui/MyWidgets/app/src/main/java/com/rhbekti/mywidgets/RandomNumberWidget.kt
@@ -0,0 +1,41 @@
+package com.rhbekti.mywidgets
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class RandomNumberWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val lastUpdate = "Random: " + NumberGenerator.generate(100)
+    val views = RemoteViews(context.packageName, R.layout.random_number_widget)
+    views.setTextViewText(R.id.appwidget_text, lastUpdate)
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
