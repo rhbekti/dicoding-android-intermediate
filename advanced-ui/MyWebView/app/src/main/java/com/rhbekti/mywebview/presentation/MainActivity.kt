ca6c9c52c679c1eeee21b77a27b9b26549d099d4
rhbekti <rhbekti.ac.id@gmail.com>
2023-10-11 06:10:17 +0700

    Add: MyWebView

---
+++ advanced-ui/MyWebView/app/src/main/java/com/rhbekti/mywebview/presentation/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.rhbekti.mywebview.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import com.rhbekti.mywebview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                view.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')")
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(view: WebView, url: String, message: String, result: android.webkit.JsResult): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                result.confirm()
+                return true
+            }
+        }
+
+        webView.loadUrl("https://www.dicoding.com/")
+    }
+}
