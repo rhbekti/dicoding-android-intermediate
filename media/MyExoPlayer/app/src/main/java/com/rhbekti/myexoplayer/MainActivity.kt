ac1185b56518326b76a5968f4da21715b9c13958
Rahman Pambekti <67356011+rhbekti@users.noreply.github.com>
2023-10-12 14:14:20 +0700

    Add: MyExoPlayer (#31)

---
+++ media/MyExoPlayer/app/src/main/java/com/rhbekti/myexoplayer/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rhbekti.myexoplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.rhbekti.myexoplayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
+
+        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            exoPlayer.setMediaItem(videoItem)
+            exoPlayer.addMediaItem(audioItem)
+            exoPlayer.prepare()
+        }
+
+        binding.playerView.player = player
+
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.playerView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+}
