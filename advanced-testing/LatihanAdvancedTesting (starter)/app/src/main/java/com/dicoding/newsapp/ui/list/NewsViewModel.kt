c3457490ff5a8c1298a4d574a84e3bf79e7a59fb
Rahman Pambekti <67356011+rhbekti@users.noreply.github.com>
2023-10-29 09:32:53 +0700

    Add: Latihan Unit Testing LiveData (#46)

--- advanced-testing/LatihanAdvancedTesting (starter)/app/src/main/java/com/dicoding/newsapp/ui/list/NewsViewModel.kt
+++ advanced-testing/LatihanAdvancedTesting (starter)/app/src/main/java/com/dicoding/newsapp/ui/list/NewsViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.dicoding.newsapp.data.NewsRepository
 
 class NewsViewModel(private val newsRepository: NewsRepository) : ViewModel() {
+
     fun getHeadlineNews() = newsRepository.getHeadlineNews()
 
     fun getBookmarkedNews() = newsRepository.getBookmarkedNews()
---
+++ advanced-testing/LatihanAdvancedTesting (starter)/app/src/test/java/com/dicoding/newsapp/ui/list/NewsViewModelTest.kt
@@ -0,0 +1,60 @@
+package com.dicoding.newsapp.ui.list
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.dicoding.newsapp.data.NewsRepository
+import com.dicoding.newsapp.data.local.entity.NewsEntity
+import com.dicoding.newsapp.utils.DataDummy
+import com.dicoding.newsapp.data.Result
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+import com.dicoding.newsapp.utils.getOrAwaitValue
+import org.mockito.Mockito
+
+@RunWith(MockitoJUnitRunner::class)
+class NewsViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var newsRepository: NewsRepository
+    private lateinit var newsViewModel: NewsViewModel
+    private val dummyNews = DataDummy.generateDummyNewsEntity()
+
+    @Before
+    fun setUp() {
+        newsViewModel = NewsViewModel(newsRepository)
+    }
+
+    @Test
+    fun `when Get HeadlineNews Should Not Null and Return Success`() {
+        val expectedNews = MutableLiveData<Result<List<NewsEntity>>>()
+        expectedNews.value = Result.Success(dummyNews)
+
+        `when`(newsRepository.getHeadlineNews()).thenReturn(expectedNews)
+
+        val actualNews = newsViewModel.getHeadlineNews().getOrAwaitValue()
+        Mockito.verify(newsRepository).getHeadlineNews()
+        Assert.assertNotNull(actualNews)
+        Assert.assertTrue(actualNews is Result.Success)
+        Assert.assertEquals(dummyNews.size, (actualNews as Result.Success).data.size)
+    }
+
+    @Test
+    fun `when Network Error Should Return Error`() {
+        val headlineNews = MutableLiveData<Result<List<NewsEntity>>>()
+        headlineNews.value = Result.Error("Error")
+        `when`(newsRepository.getHeadlineNews()).thenReturn(headlineNews)
+        val actualNews = newsViewModel.getHeadlineNews().getOrAwaitValue()
+        Mockito.verify(newsRepository).getHeadlineNews()
+        Assert.assertNotNull(actualNews)
+        Assert.assertTrue(actualNews is Result.Error)
+    }
+}
