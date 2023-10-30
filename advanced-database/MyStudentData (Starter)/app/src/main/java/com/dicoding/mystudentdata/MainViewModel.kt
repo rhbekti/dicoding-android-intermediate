8af182d87486b67db0a277c4af99379cbca55049
rhbekti <rhbekti.ac.id@gmail.com>
2023-10-30 14:35:03 +0700

    Add: Latihan pre populate database

--- advanced-database/MyStudentData (Starter)/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
+++ advanced-database/MyStudentData (Starter)/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
@@ -3,18 +3,12 @@ package com.dicoding.mystudentdata
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.database.StudentAndUniversity
 import com.dicoding.mystudentdata.database.StudentWithCourse
 import com.dicoding.mystudentdata.database.UniversityAndStudent
-import kotlinx.coroutines.launch
 
 class MainViewModel(private val studentRepository: StudentRepository) : ViewModel() {
-
-    init {
-        insertAllData()
-    }
 
     fun getAllStudent(): LiveData<List<Student>> = studentRepository.getAllStudent()
 
@@ -23,10 +17,6 @@ class MainViewModel(private val studentRepository: StudentRepository) : ViewMode
     fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentRepository.getAllUniversityAndStudent()
 
     fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentRepository.getAllStudentWithCourse()
-
-    private fun insertAllData() = viewModelScope.launch {
-        studentRepository.insertAllData()
-    }
 }
 
 class ViewModelFactory(private val repository: StudentRepository) : ViewModelProvider.Factory {
