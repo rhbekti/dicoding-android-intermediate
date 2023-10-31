3f4f2730f6c05d072dcd457e148d49f4f4c0a99a
rhbekti <rhbekti.ac.id@gmail.com>
2023-10-31 09:47:27 +0700

    Add: Latihan Paging 2

--- advanced-database/LatihanRawQuery/app/src/main/java/com/dicoding/mystudentdata/database/StudentDatabase.kt
+++ advanced-database/LatihanRawQuery/app/src/main/java/com/dicoding/mystudentdata/database/StudentDatabase.kt
@@ -1,11 +1,7 @@
 package com.dicoding.mystudentdata.database
 
 import android.content.Context
-import androidx.room.AutoMigration
-import androidx.room.Database
-import androidx.room.RenameColumn
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 import androidx.room.migration.AutoMigrationSpec
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.dicoding.mystudentdata.helper.InitialDataSource
@@ -16,14 +12,13 @@ import kotlinx.coroutines.launch
     entities = [Student::class, University::class, Course::class, CourseStudentCrossRef::class],
     version = 2,
     autoMigrations = [
-        AutoMigration(from = 1, to = 2),
-        AutoMigration(from = 2, to = 3, spec = StudentDatabase.MyAutoMigration::class),
+        AutoMigration(from = 1, to = 2, spec = StudentDatabase.MyAutoMigration::class),
     ],
     exportSchema = true
 )
 abstract class StudentDatabase : RoomDatabase() {
 
-    @RenameColumn(tableName = "Student", fromColumnName = "graduate", toColumnName = "isGraduate")
+    @RenameColumn(tableName = "University", fromColumnName = "name", toColumnName = "universityName")
     class MyAutoMigration : AutoMigrationSpec
 
     abstract fun studentDao(): StudentDao
